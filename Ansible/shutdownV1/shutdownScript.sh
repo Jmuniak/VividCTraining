c9f3bc765ff5201c9626be289cb31943a2ad9e16
Jeremy <jeremy.e.muniak@gmail.com>
2021-06-17 13:19:08 -0400

    script module targeting file, cd and ls working

---
+++ Ansible/shutdownV1/shutdownScript.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+date
+cd /mnt/c/Users/JeremyMuniak/Desktop/CodeVaultV1
+ls
+# Play 1
+# cd /mnt/c/Users/JeremyMuniak/Desktop/CodeVaultV1
+# git add .
+# git commit -m "Thank you Ansible Shutdown Playbook"
+##### git push, password needs to be provided ####
+# Play 2
+# cd /mnt/c/Users/JeremyMuniak/tutorials/
+# git add .
+# etc...
+# Play 3?
+# cd /mnt/c/Users/JeremyMuniak/Code/jeremy-christian-project
+# git add .
+# etc...
+
