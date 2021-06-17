f60faa198b98b5246e3a0452afcaa1b48c052897
Jeremy <jeremy.e.muniak@gmail.com>
2021-06-17 11:49:47 -0400

    local playbook, issueL sudo pw required

---
+++ Ansible/myFirstScripts/test_script.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+sudo date
+# apt-add-repository ppa:ansible/ansible -y
+# apt-get update
+# apt-get install ansible -y
+# cp /vagrant/hosts /etc/
+# cp /vagrant/ansible/hosts /etc/ansible
