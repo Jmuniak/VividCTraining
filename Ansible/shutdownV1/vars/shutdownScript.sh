5f3157b430080a6e818631626b9aeb05dc1d751e
Jmuniak <jeremy.e.muniak@gmail.com>
2021-06-17 23:20:29 -0400

    Thank you Ansible Shudown Playbook

--- Ansible/shutdownV1/vars/shutdownScript.sh
+++ Ansible/shutdownV1/vars/shutdownScript.sh
@@ -14,19 +14,6 @@ git add .
 git commit -m "Thank you Ansible Shudown Playbook"
 git push
 
-# Play 1
-# cd /mnt/c/Users/JeremyMuniak/Desktop/CodeVaultV1
-# git add .
-# git commit -m "Thank you Ansible Shutdown Playbook"
-##### git push, password needs to be provided ####
-# Play 2
-# cd /mnt/c/Users/JeremyMuniak/tutorials/
-# git add .
-# etc...
-# Play 3?
-# cd /mnt/c/Users/JeremyMuniak/Code/jeremy-christian-project
-# git add .
-# etc...
 
 # alias
 # AshutDown='cd /mnt/c/Users/JeremyMuniak/tutorials/Ansible/shutdownV1'
